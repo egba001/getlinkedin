127731d9128515f78d994b1f14c2bad56fd3a1e5
Egba Nnaemeka <egba.nnaemeka@nvit.tech>
2023-09-24 09:37:54 +0100

    fix

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -4,6 +4,10 @@ import Star from "./../common/Star";
 import bg from './../../assets/manmatrix.png';
 import CountdownTimer from "./../ui/CountdownTimer";
 import BlurredCircle from "./../common/BlurredCircle";
+import chain from './../../assets/chain.png'
+import bulb from './../../assets/bulb.png'
+import fire from './../../assets/fire.png'
+import curve from './../../assets/svg/curve.svg'
 import { motion } from "framer-motion"
 import { Link } from "react-router-dom";
 
@@ -28,7 +32,7 @@ const Header = () => {
             </div>
             <div className="flex flex-col h-full font-bold mx-auto container px-1 lg:px-10">
                 <div className="mb-14">
-                    <h2 className="text-center italic lg:text-right text-base lg:text-3xl">Igniting a Revolution in <span className="relative after:absolute after:bg-[url(curve.svg)] after:bg-center after:bg-no-repeat  after:bg-contain after:rounded-[70%] after:h-12 after:w-full after:-bottom-10 lg:after:-bottom-12 after:left-0">HR Innovation</span></h2>
+                    <h2 className="text-center italic lg:text-right text-base lg:text-3xl">Igniting a Revolution in <span className="relative ">HR Innovation<img src={curve} alt='Curve logo' className="absolute w-full -bottom-15 left-0" /></span></h2>
                 </div>
                 <div className="flex flex-col lg:flex-row justify-between relative h-full lg:h-[100vh]">
                     <motion.div
@@ -41,12 +45,18 @@ const Header = () => {
                         initial={{ opacity: 0 }}
                         className='flex flex-col lg:mt-28 items-center lg:items-start'>
                         <div>
-                            <h1 className="font-clash text-3xl lg:text-7xl font-bold relative after:absolute after:bg-[url('img/bulb.png')] after:h-[80%] after:w-[80%] after:bg-contain after:bg-no-repeat after:bottom-[80%] after:left-[87%]">getlinked Tech</h1>
+                            <h1 className="font-clash text-3xl lg:text-7xl font-bold relative">
+                                <img src={bulb} alt='Bulb icon' className="absolute bottom-[80%] w-[10%] left-[87%]" /> 
+                                <span>getlinked Tech</span></h1>
                         </div>
                         <div className="flex items-center">
                             <h2 className="font-clash lg:ml-0 text-3xl lg:text-7xl font-bold relative ">Hackathon <span className="text-purp">1.0</span></h2>
-                            <div className="lg:h-12 lg:w-12 w-8 h-8 inline-block bg-[url('img/chain.png')] bg-no-repeat bg-contain"></div>
-                            <div className="lg:h-12 lg:w-12 w-8 h-8 inline-block bg-[url('img/fire.png')] bg-no-repeat bg-contain"></div>
+                            <div className="lg:h-12 lg:w-12 w-8 h-8 inline-block">
+                                <img src={chain} alt="Chain image"className="w-full" />
+                            </div>
+                            <div className="lg:h-12 lg:w-12 w-8 h-8 inline-block">
+                                <img src={fire} alt="Chain image"className="w-full" />
+                            </div>
                         </div>
                         <p className="text-[12px] lg:text-[20px] text-center lg:text-start font-normal font-sans w-[80%] lg:w-[66%] mt-6 mb-12">Participate in getlinked tech Hackathon 2023 stand a chance to win a Big prize</p>
                         <Link to='/registration'>
